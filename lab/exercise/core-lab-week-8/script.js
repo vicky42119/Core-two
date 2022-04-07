08af3c3104906b91af2b008e9972fce60551812c
vicky42119 <91129632+vicky42119@users.noreply.github.com>
2022-04-07 18:58:29 -0400

    Update script.js

--- lab/exercise/core-lab-week-8/script.js
+++ lab/exercise/core-lab-week-8/script.js
@@ -1,14 +1,14 @@
-console.log('hi')
-fetch('https://api.airtable.com/v0/appDsgDNlMVCrpbey/Table%201'), {
+console.log('hi');
+fetch('https://api.airtable.com/v0/appDsgDNlMVCrpbey/Table%201',{
     Headers: {
         Authorization: 'Bearer keyZA1e5QRYt1En9y',
 },
-}
+})
     .then (reponse => Response.json())
     .then (data => {
         console.log(data);
 
-    const container = document.querySelector('name');
+    const container = document.querySelector('.names');
 
     data.records.forEach(name => {
         console.log(name);
@@ -19,7 +19,5 @@ fetch('https://api.airtable.com/v0/appDsgDNlMVCrpbey/Table%201'), {
                 <h5>${name.fields.date}</h5>
         </div>;
     }); 
+});
 
-    console.log(data);
-
-})
