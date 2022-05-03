211ca5eace5e2c2b974fbc0969b9d655c8d075a1
vicky42119 <91129632+vicky42119@users.noreply.github.com>
2022-05-03 17:34:08 -0400

    update

--- studio/project-3/script.js
+++ studio/project-3/script.js
@@ -1,84 +1,75 @@
-let DropdownArtistValue ='All'; // initial value
-    
+let DropdownArtistValue = 'All'; // initial value
 function handleArtistDropdown() {
   console.log(event.target.value);
-  DropdownValue = event.target.value;
+  DropdownArtistValue = event.target.value;
   generateContent();
 }
 
-let DropdownYearValue ='All'; // initial value
-    
+let DropdownYearValue = 'All'; // initial value
 function handleYearDropdown() {
   console.log(event.target.value);
-  DropdownValue = event.target.value;
+  DropdownYearValue = event.target.value;
   generateContent();
 }
 
-let DropdownMediumValue ='All'; // initial value
-    
+let DropdownMediumValue = 'All'; // initial value
 function handleMediumDropdown() {
   console.log(event.target.value);
-  DropdownValue = event.target.value;
+  DropdownMediumValue = event.target.value;
   generateContent();
 }
 
-let DropdownColorValue ='All'; // initial value
-    
+let DropdownColorValue = 'All'; // initial value
 function handleColorDropdown() {
   console.log(event.target.value);
-  DropdownValue = event.target.value;
+  DropdownColorValue = event.target.value;
   generateContent();
 }
 
-function generateContent(){
-fetch('https://api.airtable.com/v0/appDsgDNlMVCrpbey/Table%201', {
-  headers: {
-    Authorization: 'Bearer keyZA1e5QRYt1En9y', // this is your API key, starting with 'key...' found in your Airtable account
-  },
-})
-  .then(res => res.json()) // tells JS to expect data in json format
-  .then(data => {
-    // all your code should go inside here
+function generateContent() {
+  fetch('https://api.airtable.com/v0/appDsgDNlMVCrpbey/Table%201', {
+    headers: {
+      Authorization: 'Bearer keyZA1e5QRYt1En9y', // this is your API key, starting with 'key...' found in your Airtable account
+    },
+  })
+    .then(res => res.json()) // tells JS to expect data in json format
+    .then(data => {
+      // all your code should go inside here
 
-    console.log(data); // first, log out your data. Explore it in the browser console.
+      console.log(data); // first, log out your data. Explore it in the browser console.
 
+      const postersContainer = document.querySelector('.posters-container');
+      // tell JS about the div we added to our html file so we can put content inside it
+      postersContainer.innerHTML = '';
+      // loop over each record (row) of our Airtable data
 
-    
-    const postersContainer = document.querySelector('.posters-container');
-    // tell JS about the div we added to our html file so we can put content inside it
+      data.records
+        .slice(0, 70) // only show the first 5 albums
 
-    // loop over each record (row) of our Airtable data
+        .forEach(poster => {
+          console.log(poster); // look in the console at each album to see what fields we can access (these are your own table headers from Airtable)
 
-    data.records
-      .slice(0, 70) // only show the first 5 albums
-      .filter(poster => {
-        return DropdownArtistValue === 'All' ? poster : poster.fields.Artist === DropdownArtistValue;
-      })
+          const shouldHideArtist =
+            poster.fields.Artist !== DropdownArtistValue && DropdownArtistValue !== 'All';
 
-      .filter(poster => {
-        return DropdownYearValue === 'All' ? poster : poster.fields.Year === DropdownYearValue;
-      })
+          const shouldHideMedium =
+            poster.fields.Medium !== DropdownMediumValue && DropdownMediumValue !== 'All';
 
-      .filter(poster => {
-        return DropdownMediumValue === 'All' ? poster : poster.fields.Medium === DropdownMediumValue;
-      })
+          const isWithinYearRange =
+            poster.fields.Year >= parseInt(DropdownYearValue) &&
+            poster.fields.Year < parseInt(DropdownYearValue) + 10;
 
-      .filter(poster => {
-        return DropdownColorValue === 'All' ? poster : poster.fields.Color === DropdownColorValue;
-      })
+          const shouldHideYear = !isWithinYearRange && DropdownYearValue !== 'All';
 
-
-      .forEach(poster => {
-        console.log(poster); // look in the console at each album to see what fields we can access (these are your own table headers from Airtable)
-
-        // now we add html to our albumsContainer div
-        // this is where we take our data from Airtable and put it in our html
-        // think of this chunk of HTML as a component template for each entry in our database
-        // note the backticks `` below. This allows us to add html + js together using ${field} in a single block of code
-        postersContainer.innerHTML += `
+          const shouldHidePoster = shouldHideArtist || shouldHideMedium || shouldHideYear;
+          // now we add html to our albumsContainer div
+          // this is where we take our data from Airtable and put it in our html
+          // think of this chunk of HTML as a component template for each entry in our database
+          // note the backticks `` below. This allows us to add html + js together using ${field} in a single block of code
+          postersContainer.innerHTML += `
       
           <div class="poster" onclick="imageClick()">
-          <div class="images">
+          <div class="images ${shouldHidePoster && 'hide'}">
           <img src="${poster.fields.Attachements[0].thumbnails.large.url}" class="img2" width='200' />
           
           </div>
@@ -92,13 +83,12 @@ fetch('https://api.airtable.com/v0/appDsgDNlMVCrpbey/Table%201', {
           
         `;
 
-        // that's it!
-        // Try adding or removing items in your Airtable base and see your website update on refresh
-      });
-    
-  });    
+          // that's it!
+          // Try adding or removing items in your Airtable base and see your website update on refresh
+        });
+    });
 }
-generateContent()
+generateContent();
 
 const screen = document.querySelector('.background-screen');
 let currentItem;
@@ -113,6 +103,4 @@ function closeScreen() {
   screen.style.display = 'none';
   currentItem.classList.remove('show-info');
 }
-
-
 
--- studio/project-4/p5.js
+++ studio/project-4/p5.js
@@ -1,31 +1,30 @@
-let rectWidth;
+let theShader;
+// this variable will hold our webcam video
+let cam;
+
+function preload(){
+  // load the shader
+  theShader = loadShader('project-4/shader.vert', 'project-4/shader.frag');
+}
 
 function setup() {
-  createCanvas(720, 400);
+  // shaders require WEBGL mode to work
+  createCanvas(710, 400, WEBGL);
   noStroke();
-  background(230);
-  rectWidth = width / 4;
+
+  cam = createCapture(VIDEO);
+  cam.size(710, 400);
+
+  cam.hide();
 }
 
 function draw() {
-  // keep draw() here to continue looping while waiting for keys
-}
+  // shader() sets the active shader with our shader
+  shader(theShader);
 
-function keyPressed() {
-  let keyIndex = -1;
-  if (key >= 'a' && key <= 'z') {
-    keyIndex = key.charCodeAt(0) - 'a'.charCodeAt(0);
-  }
-  if (keyIndex === -1) {
-    // If it's not a letter key, clear the screen
-    background(230);
-  } else {
-    // It's a letter key, fill a rectangle
-    randFill_r = Math.floor(Math.random() * 255 + 1);
-    randFill_g = Math.floor(Math.random() * 255 + 1);
-    randFill_b = Math.floor(Math.random() * 255 + 1);
-    fill(randFill_r, randFill_g, randFill_b);
-    let x = map(keyIndex, 0, 25, 0, width - rectWidth);
-    rect(x, 0, rectWidth, height);
-  }
+  // passing cam as a texture
+  theShader.setUniform('tex0', cam);
+
+  // rect gives us some geometry on the screen
+  rect(0,0,width,height);
 }
