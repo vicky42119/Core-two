a24ee03d2b894f4344ff76c192b191437b0018dd
vicky42119 <91129632+vicky42119@users.noreply.github.com>
2022-05-08 16:56:00 -0400

    update

--- studio/project-4/sketch.js
+++ studio/project-4/sketch.js
@@ -11,7 +11,7 @@ let rectHeight;
 let userTypingBool = false;
 
 function setup() {
-  createCanvas(1000, 600);
+  createCanvas(1420, 900);
   noStroke();
   background(255);
   rectWidth = 100;
@@ -90,9 +90,9 @@ function randomShape() {
     fill(randFill_r, randFill_g, randFill_b);
     let x = map(keyIndex, 0, 25, 0, width - rectWidth);
     randomRange(0,1);
-    if (randomRange(0,1) == 0) {
+    if (randomRange(0,1) == 1) {
       ellipse(x, 0, rectWidth, height);
-    }if (randomRange(0,1) == 1){
+    }if (randomRange(0,1) == 0){
       rect(x, 0, rectWidth, height);
     }
 }
