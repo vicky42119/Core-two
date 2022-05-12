7f0914605cacf15349bee62f9b366117e432e214
vicky42119 <91129632+vicky42119@users.noreply.github.com>
2022-05-12 17:15:15 -0400

    update

---
+++ design_studies/script.js
@@ -0,0 +1,111 @@
+
+//getting the canvas from html
+const canvas = document.getElementById('canv');
+//sizing the canvas
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const ctx = canvas.getContext('2d');
+let cols = Math.floor(window.innerWidth / 20) + 1;
+let ypos = Array(cols).fill(0);
+
+ctx.fillStyle = '#000'; //canvas background color
+ctx.fillRect(0, 0, canvas.width, canvas.height); //I commented this line out and nothing changed
+
+function matrix () {
+    const w = window.innerWidth;
+    const h = window.innerHeight;
+
+    if (canvas.width !== w) {
+        canvas.width = w;
+        cols = Math.floor(window.innerWidth / 20) + 1;
+        ypos = Array(cols).fill(0);
+    }
+    if (canvas.height !== h) {
+        canvas.height = h;
+    }
+
+    ctx.fillStyle = '#0001';
+    ctx.fillRect(0, 0, w, h);
+
+    ctx.fillStyle = '#949494';
+    ctx.font = '15pt monospace';
+
+    ypos.forEach((y, ind) => {
+        const text = String.fromCharCode(Math.random() * 10);
+        const x = ind * 20;
+        ctx.fillText(text, x, y);
+        if (y > 100 + Math.random() * 10000) ypos[ind] = 0;
+        else ypos[ind] = y + 20;
+    });
+}
+
+setInterval(matrix, 50);
+
+
+
+
+
+
+
+//setting up carousel
+const slider = document.querySelector('.read-more')
+const slider2 = document.querySelector('.read-more2')
+const slider3 = document.querySelector('.read-more3')
+const slider4 = document.querySelector('.read-more4')
+const rightArrow = document.querySelectorAll('.arrow-right');
+const leftArrow = document.querySelectorAll('.arrow-left');
+
+
+console.log(rightArrow);
+
+for (let i = 0; i < rightArrow.length; i ++) {
+
+    rightArrow[i].addEventListener('click', () => {
+
+
+        if (i == 0){
+            slider.style.transform = `translate(${-100}%)`
+    
+        }
+        if (i == 1){
+            slider2.style.transform = `translate(${-100}%)`
+    
+        }
+        if (i == 2){
+            slider3.style.transform = `translate(${-100}%)`
+    
+        }
+        if (i == 3){
+            slider4.style.transform = `translate(${-100}%)`
+    
+        }
+    })
+}
+
+
+for (let i = 0; i < leftArrow.length; i ++) {
+
+    leftArrow[i].addEventListener('click', () => {
+
+        if (i == 0){
+            slider.style.transform = `translate(${100}%)`
+    
+        }
+        if (i == 1){
+            slider2.style.transform = `translate(${100}%)`
+    
+        }
+        if (i == 2){
+            slider3.style.transform = `translate(${100}%)`
+    
+        }
+        if (i == 3){
+            slider4.style.transform = `translate(${100}%)`
+    
+        }
+    })
+}
+
+
+
